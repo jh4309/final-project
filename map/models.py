532e87cf9206f8fa1dee2bd85fc26c24ec6aa97b
jh4309 <jh4309@columbia.edu>
2020-10-12 09:19:14 +0000

    add models

--- map/models.py
+++ map/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
+class sighting (models.Model):
+    latitude = models.DecimalField(
+            max_digits=10, 
+            decimal_places=4,
+    )
+
+
+    longitude = models.DecimalField(
+            max_digits=10,
+            decimal_places=4,
+    )
+
+    PM = 'PM'
+    AM = 'AM'
+
+    SHIFT_CHOICES = [
+            (PM, ('PM')),
+            (AM, ('AM')),
+    ]
+
+
+    shift = models.CharField(
+            max_length=15,
+            choices=SHIFT_CHOICES,
+            default=AM,
+    )
+
+
+    date = models.DateField(
+            help_text=('Date')
+    )
+
+    age = models.IntegerField()
+
+
+
+
 # Create your models here.
