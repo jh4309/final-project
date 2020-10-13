2d4e532c97f05f414d8594e16f6ae86689e70a0f
jh4309 <jh4309@columbia.edu>
2020-10-13 16:18:11 +0000

    adding new view in detail

--- map/urls.py
+++ map/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('<int:map_id>/', views.detail, name='detail'),
 ]
--- map/views.py
+++ map/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 
 from .models import sighting
 
@@ -12,5 +13,17 @@ def index(request):
    
               }
     return render(request, 'map/index.html', context)
+
+def detail(request, map_id):
+    sighting = get_object_or_404(sighting, pk=map_id)
+
+    context = {
+            'sighting': sighting,
+    }
+
+    return render(request, 'map/detail.html', context)
+
+
+
 
 # Create your views here.
