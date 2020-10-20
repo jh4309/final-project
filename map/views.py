47390813078158964e29f5ab9492c6a88bb5c8dc
Baoziz <72250650+Baoziz@users.noreply.github.com>
2020-10-20 04:12:56 -0400

    Update views.py

--- map/views.py
+++ map/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.shortcuts import redirect
 from .models import sighting
+from .forms import updateform
 
 
 
